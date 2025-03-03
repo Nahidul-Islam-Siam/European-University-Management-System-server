1deb7f0f2c7b8c008c708817b2d4cefe884fbb08
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-03-03 19:34:40 +0600

    commit fist time for server

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// eslint.config.mjs
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["node_modules/", "dist/", "**/*.js"]
+  },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error"
+    }
+  }
+];
