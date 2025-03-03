46fa68b1b497c9958469552af113ddffdf591f50
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-03-03 22:43:15 +0600

    added prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,39 @@
-// eslint.config.mjs
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
+  eslintPluginPrettierRecommended,
   {
-    ignores: ["node_modules/", "dist/", "**/*.js"]
+    ignores: ["node_modules/", "dist/", "**/*.js"],
   },
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        process: "readonly",
+      },
+    },
     rules: {
       "no-unused-vars": "error",
       "no-unused-expressions": "error",
       "prefer-const": "error",
-      "no-console":"warn",
+      "no-console": "warn",
       "no-undef": "error",
-  
     },
-    globals: {
-      "process": "readonly",
-    }
-
-  }
+  },
 ];
