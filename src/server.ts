0905c07773b7d06d171952ad6a175cd3a30b3c86
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-03-03 23:56:10 +0600

    Refactor server and app configuration to use environment variables and improve logging

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+
 import app from './app';
 import config from './app/config';
 
@@ -6,7 +7,7 @@ async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(` app listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
