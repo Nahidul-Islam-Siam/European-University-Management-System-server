4dfb04ca5796013053d742cdcabeb7719a33c763
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-03-04 15:38:45 +0600

    Clean up app.ts by removing unnecessary whitespace

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,8 @@ import cors from 'cors';
 import { StudentRoutes } from './app/modules/student/student.route';
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
-
-
 
 app.use('/api/v1/students', StudentRoutes);
 
