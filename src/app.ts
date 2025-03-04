c684dc7359a802eeba6c4ec4844bf10ec3c7424a
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-03-04 12:45:03 +0600

    sucessuffuly interted data to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app = express();
 
 // parser
 app.use(express.json());
 app.use(cors());
+
+
+// api/v1/students/create-student
+// application routes
+
+
+app.use('/api/v1/students',StudentRoutes)
 
 const getAController =  (req: Request, res: Response) => {
   const a = 10;
